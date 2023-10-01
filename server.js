1fdb532eb8760629ae53034d2fc8255c520b57a5
Anurag2426 <chemistry2426@gmail.com>
2023-10-01 14:50:46 +0530

    deployment error solve

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import authRoutes from './routes/authRoute.js';
 import connectDB from './config/db.js';
-import cors from 'cors';
-import categoryRoutes from './routes/categoryRoutes.js';
+import cors from 'cors'
+import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -25,7 +26,7 @@ const __dirname=path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(process.cwd(),"./client/build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
@@ -33,10 +34,10 @@ app.use("/api/v1/category",categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
 
-// Serve the index.html file from the client/build directory
-app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), './client/build/index.html'));
-});
+// rest api
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 
 
 
@@ -45,5 +46,5 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
